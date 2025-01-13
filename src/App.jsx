bd420219bff87bf3d446a1d0689a60c1a7f9c923
jomaca92 <jomaca92@gmail.com>
2025-01-12 19:21:47 -0600

    added tailwind and shadcn config

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className="flex flex-col items-center justify-center h-screen space-y-5">
+      <div className="flex flex-row items-center justify-center gap-5">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,19 +16,19 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-2xl font-bold">Vite + React</h1>
+      <div className="flex flex-col items-center justify-center gap-5">
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-sm text-gray-500">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
